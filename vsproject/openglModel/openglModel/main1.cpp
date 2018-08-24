355a7b3c9425cf974082f549041d2b91cac385ab
PengYanY <18779735058@163.com>
2018-08-24 19:53:03 +0800

    模型导入

---
+++ vsproject/openglModel/openglModel/main1.cpp
@@ -0,0 +1,18 @@
+#ifndef  STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#endif
+#include "model.h"
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <camera.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+
+	std::cout << "hello";
+	return 0;
+}
